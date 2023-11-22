bcdf67a8a7adbe632c766841feceb3c73d762473
vedasole <vedasole001@gmail.com>
2023-11-22 13:54:03 +0530

    Added Resume

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,21 +3,23 @@ import logo from '../assets/images/logo.png'
 
 const Navbar = () => {
   return (
-  <nav className="py-2 text-white flex justify-between bg-[#051726] min-w-full px-12 md:px-28 sticky top-0 z-50">
-    <a href="#">
-      <img src={logo} className="h-12 pt-3 cursor-pointer" alt='Ved Asole' />
-    </a>
-    <ul className="py-4 flex space-x-11 justify-end font-medium text-lg">
-      <li className="cursor-pointer hover:text-[#81FFD9] hidden md:flex">
-        <a href="#">Home</a>
-      </li>
-      <li className="cursor-pointer hover:text-[#81FFD9] hidden lg:flex">
-        <a href="#projects">Projects</a>
-      </li>
-      <li className="cursor-pointer hover:text-[#81FFD9]">Resume</li>
-      <li className="cursor-pointer hover:text-[#81FFD9] hidden lg:flex">Search</li>
-    </ul>
-  </nav>
+    <nav className="py-2 text-white flex justify-between bg-[#051726] min-w-full px-12 md:px-28 sticky top-0 z-50">
+      <a href="#">
+        <img src={logo} className="h-12 pt-3 cursor-pointer" alt='Ved Asole' />
+      </a>
+      <ul className="py-4 flex space-x-11 justify-end font-medium text-lg">
+        <li className="cursor-pointer hover:text-[#81FFD9] hidden md:flex">
+          <a href="#">Home</a>
+        </li>
+        <li className="cursor-pointer hover:text-[#81FFD9] hidden lg:flex">
+          <a href="#projects">Projects</a>
+        </li>
+        <li className="cursor-pointer hover:text-[#81FFD9]">
+          <a href="/Ved_Asole_Java_Developer_CV_Final.pdf">Resume</a>
+        </li>
+        <li className="cursor-pointer hover:text-[#81FFD9] hidden lg:flex">Search</li>
+      </ul>
+    </nav>
 
   )
 }
